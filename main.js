a58d91b2313ddf4fcd3f940578f06d0755776bd5
ItzArima <skaypaprilia@gmail.com>
2021-10-13 16:23:29 +0200

    js commentato

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-
+//impostiamo il for
 for (let i=1; i<=100; i++){
-    
+    //fizz ogni 3 buzz ogni 5
     var fizz = i % 3;
-    console.log(fizz);
     var buzz = i % 5;
-    if (fizz == 0){
+    //scriviamo fizz
+    if (fizz == 0){ 
+        //scriviamo fizzbuzz se      
         if(buzz == 0){
             var p = document.createElement ("p");
             p.id = 'fizzbuzz';
@@ -16,16 +17,19 @@ for (let i=1; i<=100; i++){
             p.id = 'fizz';
         }
     }
+    //scriviamo buzz
     else if(buzz == 0){
         var p = document.createElement ("p");
         var content = document.createTextNode("Buzz");
         p.id = 'buzz';
     }
+    //impostiamo il valore a standard
     else{
         var p = document.createElement ("p");
         var content = document.createTextNode(i)
         p.id = 'standard';
     }
+    //impostiamo il valore dei blocchi stardard e inseriamo tutto nell'html
     const square = document.createElement ("div")
     square.appendChild(p);
     p.appendChild(content);
