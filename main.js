a5b165b585fc80ff405e29f3e07e6114a968e1d9
ItzArima <skaypaprilia@gmail.com>
2021-10-13 16:13:35 +0200

    esercizio completato

--- main.js
+++ main.js
@@ -7,20 +7,24 @@ for (let i=1; i<=100; i++){
     if (fizz == 0){
         if(buzz == 0){
             var p = document.createElement ("p");
+            p.id = 'fizzbuzz';
             var content = document.createTextNode("Fizzbuzz");
         }
         else{
             var p = document.createElement ("p");
             var content = document.createTextNode("Fizz");
+            p.id = 'fizz';
         }
     }
     else if(buzz == 0){
         var p = document.createElement ("p");
         var content = document.createTextNode("Buzz");
+        p.id = 'buzz';
     }
     else{
         var p = document.createElement ("p");
         var content = document.createTextNode(i)
+        p.id = 'standard';
     }
     const square = document.createElement ("div")
     square.appendChild(p);
